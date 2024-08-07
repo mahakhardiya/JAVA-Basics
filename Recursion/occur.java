926c1007814e301935e75e289aea52b469672bd8
Mahak <mahakhardiya@gmail.com>
2024-08-07 15:23:22 +0530

    Check Occurence

--- Recursion/occur.java
+++ Recursion/occur.java
@@ -10,7 +10,7 @@ public class occur {
     }
     public static void main(String[] args) {
         int arr[] = {10, 20, 30, 40, 50};
-        int prr[] = {32, 43, 12, 87, 16};
+        int prr[] = {32, 43, 12, 87, 16, 43};
         System.out.println(checkOccurence(arr, 12, 0));
         System.out.println(checkOccurence(prr, 43, 0));
     }   
