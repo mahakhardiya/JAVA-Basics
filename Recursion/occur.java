1dac1077087e43562581e35cf50f2c6c759b89e0
Mahak <mahakhardiya@gmail.com>
2024-08-08 21:17:07 +0530

    Recursion

--- Recursion/occur.java
+++ Recursion/occur.java
@@ -1,17 +1,32 @@
 public class occur {
-    public static int checkOccurence(int arr[], int key, int i){
-        if(i == arr.length){
+    public static int checkOccurence(int arr[], int key, int i) {
+        if (i == arr.length) {
             return -1;
         }
-        if (arr[i] == key){
+        if (arr[i] == key) {
             return i;
         }
-        return checkOccurence(arr, key, i+1);
+        return checkOccurence(arr, key, i + 1);
     }
+
+    public static int lastOccurence(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lastOccurence(arr, key, i + 1);
+        if (isFound != -1) {
+            return isFound;
+        }
+        if (arr[i] == key) {
+            return i;
+        }
+        return isFound;
+    }
+
     public static void main(String[] args) {
-        int arr[] = {10, 20, 30, 40, 50};
-        int prr[] = {32, 43, 12, 87, 16, 43};
+        int arr[] = { 10, 20, 30, 40, 50 };
+        int prr[] = { 32, 43, 12, 87, 16, 43 };
         System.out.println(checkOccurence(arr, 12, 0));
         System.out.println(checkOccurence(prr, 43, 0));
-    }   
+    }
 }
---
+++ Recursion/power.java
@@ -0,0 +1,12 @@
+public class power {
+    public static int pow(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        int power = x * pow(x, n-1);
+        return power;
+    }
+    public static void main(String[] args) {
+        
+    }    
+}
