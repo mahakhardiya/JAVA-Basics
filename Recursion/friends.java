715eeb66c7f3fba709e50b08b516762782a3ba09
Mahak <mahakhardiya@gmail.com>
2024-09-06 00:01:58 +0530

    Correction

--- Recursion/friends.java
+++ Recursion/friends.java
@@ -1,5 +1,5 @@
 class friends {
-        public int[] twoSum(int[] nums, int target) {
+        public static int[] twoSum(int[] nums, int target) {
             int[] p = new int[2];
             for(int i=0; i<nums.length; i++){
                 for(int j=i+1; j<nums.length; j++){
